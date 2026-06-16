fc05ff2db65c134b50b20521b8e50b9d5a137639
Pablo Gareis <146157552+gareispablotecnica@users.noreply.github.com>
2026-06-16 08:13:01 -0300

    7.1

--- Project/Api/views.py
+++ Project/Api/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import *
+from .models import *
 
 # def ---> Funcion
 # ---> Request : Retorna la Pagina Web
@@ -29,3 +30,20 @@ def VerProductos(request):
         'VerAlumnos':query
     }
     return render(request,'Pages/Alumnos.html',data)
+
+
+def Modificacion(request,ID_Alumno):
+    query=get_object_or_404(Alumnos, ID_Alumno=ID_Alumno)
+    data={
+        'Formulario':FormularioRegistro(instance=query)
+    }
+    # --->Si el metodo de HTML es post
+    if request.method=="POST":
+        # --> Guarda lo que registro a la Base de DATOS
+        query=FormularioRegistro(data=request.POST,instance=query,files=request.FILES)
+        if query.is_valid():
+            query.save()
+            data["Mensaje"]="Datos Modificados"
+        else:
+            data['Mensaje']="No se pudo Registrar"
+    return render(request,'Pages/Registro.html',data)
